e6c87b4c3694a3170e108c10e00db70eebd2dce5
Mihir Prabhudesai <mihirp1998.mp@gmail.com>
2018-10-01 02:34:36 +0400

    changes

--- model.py
+++ model.py
@@ -204,12 +204,11 @@ class cyclegan(object):
 
                 counter += 1
 
-                print(("Epoch: [%2d] [%4d/%4d] time: %4.4f" % (
-                    epoch, idx, batch_idxs, time.time() - start_time)))
+                
 
                 if np.mod(counter, args.print_freq) == 1:
                     self.sample_model(args.sample_dir, epoch, idx)
-
+                    print(("Epoch: [%2d] [%4d/%4d] time: %4.4f" % (epoch, idx, batch_idxs, time.time() - start_time)))
                 if np.mod(counter, args.save_freq) == 2:
                     self.save(args.checkpoint_dir, counter)
 
