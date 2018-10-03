de3e9ed4a0bf94171b02554dacf1f6c8eb58fa05
Mihir Prabhudesai <mihirp1998.mp@gmail.com>
2018-10-03 21:57:36 +0400

    model updated

--- module.py
+++ module.py
@@ -25,10 +25,12 @@ def discriminator(image, options,is_training, reuse=False, name="discriminator")
         # h3_flat = tf.layers.flatten(h3,name="flatten")
 
         h0 = lrelu(conv2dn(image, 64, name='d_h0_conv'))
-        h1 = lrelu(batch_normn(name='d_bn1')(conv2dn(h0, 64*2, name='d_h1_conv')))
-        h2 = lrelu(batch_normn(name='d_bn2')(conv2dn(h1, 64*4, name='d_h2_conv')))
-        h3 = lrelu(batch_normn(name='d_bn3')(conv2dn(h2, 64*8, name='d_h3_conv')))
-        h3 = lrelu(batch_normn(name='d_bn4')(conv2dn(h3, 64*8, name='d_h4_conv')))
+
+        h1 = lrelu(batch_normn(name='d_bn1')(conv2dn(h0, 64*2, name='d_h1_conv'),is_training))
+        h2 = lrelu(batch_normn(name='d_bn2')(conv2dn(h1, 64*4, name='d_h2_conv'),is_training))
+        h3 = lrelu(batch_normn(name='d_bn3')(conv2dn(h2, 64*8, name='d_h3_conv'),is_training))
+        h3 = lrelu(batch_normn(name='d_bn4')(conv2dn(h3, 64*8, name='d_h4_conv'),is_training))
+
         print(h3)
         flat = tf.layers.flatten(h3,name="flatten")
         print(flat)
@@ -106,6 +108,48 @@ def generator_unet(image, options, reuse=False, name="generator"):
         return tf.nn.tanh(d8)
 
 
+# def generator_resnet(image, options,is_training, reuse=False, name="generator"):
+
+#     with tf.variable_scope(name):
+#         # image is 256 x 256 x input_c_dim
+#         if reuse:
+#             tf.get_variable_scope().reuse_variables()
+#         else:
+#             assert tf.get_variable_scope().reuse is False
+
+#         def residule_block(x, dim, ks=3, s=1, name='res'):
+#             y = conv2d(x,dim, k_h=5, k_w=5, d_h=1, d_w=1, name=name+'_c1')
+#             y = batch_norm(y,is_training, name+'_bn1')
+#             y = tf.nn.relu(y)
+#             y = conv2d(y, dim, ks, s, padding='SAME', name=name+'_c2')
+#             y = batch_norm(y,is_training,name+ '_bn2')
+#             return y + x
+
+
+#         c1 = tf.nn.relu(batch_norm(conv2d(image, 64, 7, 1, padding='SAME', name='init_conv',use_bias=False),is_training, 'init_bn'))
+
+        
+#         r1 = residule_block(c1, 64, name='g_r1')
+#         r2 = residule_block(r1, 64, name='g_r2')
+#         r3 = residule_block(r2, 64, name='g_r3')
+#         r4 = residule_block(r3, 64, name='g_r4')
+#         r5 = residule_block(r4, 64, name='g_r5')
+#         r6 = residule_block(r5, 64, name='g_r6')
+#         r7 = residule_block(r6, 64, name='g_r7')
+#         r8 = residule_block(r7, 64, name='g_r8')
+#         r9 = residule_block(r8, 64, name='g_r9')
+
+#         c2 = tf.nn.relu(batch_norm(conv2d(image, 64, 3, 1, padding='SAME', name='first_conv',use_bias=False),is_training,'first_bn'))
+#         d1 = deconv2d(c2, 256, 3, 2, name='g_d1_dc1')
+#         d1 = tf.nn.relu(d1)
+#         d2 = deconv2d(d1, 256, 3, 2, name='g_d2_dc')
+#         d2 = tf.nn.relu(d2)
+
+#         pred = tf.nn.tanh(conv2d(d2, 3, 4, 1, padding='SAME', name='g_pred_c'))
+
+#         return pred
+
+
 def generator_resnet(image, options,is_training, reuse=False, name="generator"):
 
     with tf.variable_scope(name):
@@ -116,15 +160,16 @@ def generator_resnet(image, options,is_training, reuse=False, name="generator"):
             assert tf.get_variable_scope().reuse is False
 
         def residule_block(x, dim, ks=3, s=1, name='res'):
-            y = conv2d(x, dim, ks, s, padding='SAME', name=name+'_c1',use_bias=False)
-            y = batch_norm(y,is_training, name+'_bn1')
+            y = conv2dn(x,dim, k_h=5, k_w=5, d_h=1, d_w=1, name=name+'_c1')
+            y = batch_normn(name = name+'_bn1')(y,is_training)
             y = tf.nn.relu(y)
-            y = conv2d(y, dim, ks, s, padding='SAME', name=name+'_c2',use_bias = False)
-            y = batch_norm(y,is_training,name+ '_bn2')
+            y = conv2dn(y,dim, k_h=5, k_w=5, d_h=1, d_w=1, name=name+'_c2')
+            y = batch_normn(name = name+'_bn2')(y,is_training)
+
             return y + x
 
 
-        c1 = tf.nn.relu(batch_norm(conv2d(image, 64, 7, 1, padding='SAME', name='init_conv',use_bias=False),is_training, 'init_bn'))
+        c1 = tf.nn.relu(batch_normn(name = 'init_bn')(conv2dn(image,64, k_h=5, k_w=5, d_h=1, d_w=1, name='init_conv'),is_training))
 
         
         r1 = residule_block(c1, 64, name='g_r1')
@@ -137,15 +182,18 @@ def generator_resnet(image, options,is_training, reuse=False, name="generator"):
         r8 = residule_block(r7, 64, name='g_r8')
         r9 = residule_block(r8, 64, name='g_r9')
 
-        c2 = tf.nn.relu(batch_norm(conv2d(image, 64, 3, 1, padding='SAME', name='first_conv',use_bias=False),is_training,'first_bn'))
-        d1 = deconv2d(c2, 256, 3, 2, name='g_d1_dc1')
+        c2 = tf.nn.relu(batch_normn(name ='first_bn')(conv2dn(image, 64, k_h=5, k_w=5, d_h=1, d_w=1, name='first_conv'),is_training))
+        d1 = deconv2dn(c2,[tf.shape(c2)[0],64,64,256], name='g_d1_dc1')
+        d1 = batch_normn(name = name+'_bn3')(d1,is_training)
         d1 = tf.nn.relu(d1)
-        d2 = deconv2d(d1, 256, 3, 2, name='g_d2_dc')
+        d2 = deconv2dn(d1,[tf.shape(d1)[0],128,128,256], name='g_d2_dc')
+        d2 = batch_normn(name = name+'_bn4')(d2,is_training)
         d2 = tf.nn.relu(d2)
-
-        pred = tf.nn.tanh(conv2d(d2, 3, 4, 1, padding='SAME', name='g_pred_c'))
+        pred = tf.nn.tanh(conv2dn(d2, 3, k_h=5, k_w=5, d_h=1, d_w=1,name='g_pred_c'))
 
         return pred
+
+
 
 def abs_criterion(in_, target):
     return tf.reduce_mean(tf.abs(in_ - target))
